34a3fbd4c6bd3a470c138633e9cbda3bab6503a9
Luis Hernandez <luishrd@gmail.com>
2019-11-14 10:03:35 -0700

    fixes /hobbits 500 error

--- api/server.js
+++ api/server.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const Hobbits = require('../hobbits/hobbitsModel.js');
+const Hobbits = require("../hobbits/hobbitsModel.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
 });
 
-server.get('/hobbits', (req, res) => {
+server.get("/hobbits", (req, res) => {
   Hobbits.getAll()
     .then(hobbits => {
-      res.status(200).json(rows);
+      res.status(200).json(hobbits);
     })
     .catch(error => {
       res.status(500).json(error);
