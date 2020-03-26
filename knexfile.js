7193207d1ae180be1455e984e60a0f9cba3131df
ajmerr13 <oliverclosoff07@gmail.com>
2020-03-26 13:22:15 -0400

    knexfile updated

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,19 @@ module.exports = {
       directory: "./data/seeds",
     },
   },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/hobbits.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
   testing: {
     client: "sqlite3",
     connection: {
