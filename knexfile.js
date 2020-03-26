ab99da9ee84f3a29561d8a08eec42c9b197ce6e1
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-03-26 12:15:10 -0500

    test

--- knexfile.js
+++ knexfile.js
@@ -2,27 +2,40 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/hobbits.db3",
+      filename: "./data/hobbits.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations",
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: "./data/seeds",
+      directory: "./data/seeds"
+    }
+  },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/hobbits.db3"
     },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   },
   testing: {
     client: "sqlite3",
     connection: {
-      filename: "./data/test.db3",
+      filename: "./data/test.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations",
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: "./data/seeds",
-    },
-  },
+      directory: "./data/seeds"
+    }
+  }
 };
