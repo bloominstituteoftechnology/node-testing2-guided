920cb0e5d3693a6dd0fbeef003fe9925a19e9730
Luis Hernandez <luishrd@gmail.com>
2020-03-26 08:38:00 -0600

    updates dependencies

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,28 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/hobbits.db3',
+      filename: "./data/hobbits.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds",
     },
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/test.db3',
+      filename: "./data/test.db3",
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds",
     },
   },
 };
