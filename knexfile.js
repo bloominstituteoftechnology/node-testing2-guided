22cb176caf5782b61da0256c923b4366b2f8dc5a
Luis Hernandez <luishrd@gmail.com>
2020-05-21 09:32:32 -0600

    adds production config for knex

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits";
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -18,6 +20,21 @@ module.exports = {
       filename: "./data/test.db3",
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: "./data/migrations",
     },
