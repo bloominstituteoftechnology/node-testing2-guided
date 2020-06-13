775911e7f85e78f46add0e45a4fbe26d18ab781a
Veto Ramirez <vetorobert3@gmail.com>
2020-06-13 08:13:12 -0700

    Still need to work out bugs in tests. @ 1:20 something. About to go to pg.

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,15 +1,27 @@
-const db = require('../data/dnConfig.js');
+const db = require('../data/dbConfig.js');
 const { intersect } = require('../data/dbConfig.js');
 const Hobbits = ('./hobbitsModel.js');
 
 describe('hobbits model', () => {
   describe('insert()', () => {
-    it('should insert the provided hobbits into th DB', async => {
+    it('should insert the provided hobbits into th DB', async () => {
       await Hobbits.insert({name: 'gaffer'});
       await Hobbits.insert({name: 'sam'});
 
       const hobbits = await db('hobbits');
       expect(hobbits).toHaveLength(2)
+    });
+
+    it('should return what was inserted', async () => {
+      let hobbit = await Hobbits.insert({ name: 'gaffer' });
+      expect(hobbit.name).toBe('gaffer');
+      
+      hobbit = await Hobbits.insert({ name: 'sam' });
+      expect(hobbit.name).toBe('sam');
+    });
+
+    beforeEach(async () => {
+      await db('hobbits').truncate();
     })
   })
 })
