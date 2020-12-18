780abeea3c28c58c970987afd29f92283b588825
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-18 10:38:10 +0100

    package.json

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,23 @@
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits";
-
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/hobbits.db3",
-    },
+    client: 'sqlite3',
     useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    connection: {
+      filename: './data/hobbits.db3',
     },
   },
   testing: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/test.db3",
-    },
+    client: 'sqlite3',
     useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    connection: {
+      filename: './data/test.db3',
     },
   },
-
   production: {
-    client: "pg",
-    connection: pgConnection,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+
   },
 };
