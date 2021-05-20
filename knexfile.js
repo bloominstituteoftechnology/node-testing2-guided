6e0753235bf95ca4dbc8a6e2efd3559e440b1daa
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-20 17:17:30 +0200

    upgrades

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,19 @@
+const common = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
+
 module.exports = {
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
+    ...common,
     connection: {
       filename: './data/hobbits.db3',
     },
   },
   testing: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
+    ...common,
     connection: {
       filename: './data/test.db3',
     },
