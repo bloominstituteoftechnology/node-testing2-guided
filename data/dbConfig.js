37e14a6c7a93b1f676dc2457781fb97534056535
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-16 17:31:08 +0200

    use NODE_ENV

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 
 const config = require("../knexfile.js");
 
-const environment = process.env.DB_ENV || "development";
+const environment = process.env.NODE_ENV || "development";
 
 module.exports = knex(config[environment]);
