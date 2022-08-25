5ffed40a24b2590cc046d3cd2d9d1bd0ca854bb3
James Blacklock <jamesblacklock@protonmail.com>
2022-08-25 12:18:04 -0400

    committing to "lecture"

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -5,6 +5,3 @@ const config = require("../knexfile.js");
 const environment = process.env.NODE_ENV || "development";
 
 module.exports = knex(config[environment]);
-
-console.log(environment);
-console.log(config[environment]);
