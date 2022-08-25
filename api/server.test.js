a86bc2ae1843a04a7f1e388f5110148ec9ff9269
James Blacklock <jamesblacklock@protonmail.com>
2022-08-25 13:12:32 -0400

    committing to "lecture"

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,7 @@
 const db = require('../data/dbConfig');
 const Hobbit = require('./hobbits/hobbits-model');
+const server = require('./server');
+const request = require('supertest');
 
 test('check environment', () => {
     expect(process.env.NODE_ENV).toBe('testing');
@@ -63,4 +65,21 @@ describe('hobbit model tests', () => {
         result = await Hobbit.remove(3);
         expect(result).not.toBeDefined();
     });
+});
+
+describe('HTTP endpoint tests', () => {
+    test('GET /', async () => {
+        const result = await request(server).get('/');
+        console.log();
+    });
+
+    // test('GET /hobbits', async () => {});
+
+    // test('GET /hobbits/:id', async () => {});
+
+    // test('POST /hobbits', async () => {});
+
+    // test('PUT /hobbits/:id', async () => {});
+
+    // test('DELETE /hobbits/:id', async () => {});
 });
